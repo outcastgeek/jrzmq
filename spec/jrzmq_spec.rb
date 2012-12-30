f0325a5b3aa1e6479da283ea6385111ec0d459d6
outcastgeek <outcastgeek+git@gmail.com>
2012-12-29 18:05:27 -0600

    Fix for hanging build.

--- spec/jrzmq_spec.rb
+++ spec/jrzmq_spec.rb
@@ -4,33 +4,40 @@ describe ZMQ do
   context "#UPSTREAM_DOWNSTREAM" do
     it "sends two messages UPSTREAM and receives two messages DOWNSTREAM" do
 
-      context = ZMQ::Context.new(1)
-      
-      puts "Opening connection for READ DOWNSTREAM"
-      inbound = context.socket(ZMQ::UPSTREAM)
-      inbound.bind("ipc://localconnection")
-      #inbound.set_receive_time_out 10
-
-      puts "Opening connection for WRITE UPSTREAM"
-      outbound = context.socket(ZMQ::DOWNSTREAM)
-      outbound.connect("ipc://localconnection")
-      #outbound.set_send_time_out 10
-
       hello = "Hello World!"
       quit = "QUIT"
+
+      received_msg1 = nil
+      received_msg2 = nil
       
-      outbound.send(hello)
-      outbound.send(quit)
+      updown = Thread.new do
+        context = ZMQ::Context.new(1)
 
-      received_msg = inbound.recv_str
-      puts "Received #{received_msg}"
-      received_msg.should == hello
+        puts "Opening connection for READ DOWNSTREAM"
+        inbound = context.socket(ZMQ::UPSTREAM)
+        inbound.bind("ipc://localconnection")
+        #inbound.set_receive_time_out 10
 
-      received_msg = inbound.recv_str
-      puts "Received #{received_msg}"
-      received_msg.should == quit
+        puts "Opening connection for WRITE UPSTREAM"
+        outbound = context.socket(ZMQ::DOWNSTREAM)
+        outbound.connect("ipc://localconnection")
+        #outbound.set_send_time_out 10
 
-      #context.term
+        outbound.send(hello)
+        outbound.send(quit)
+
+        received_msg1 = inbound.recv_str
+        puts "Received #{received_msg1}"
+
+        received_msg2 = inbound.recv_str
+        puts "Received #{received_msg2}"
+
+        #context.term
+      end
+
+      updown.join
+      received_msg1.should == hello
+      received_msg2.should == quit
     end
   end
 end
