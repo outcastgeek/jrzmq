13eafda2f24ce405266adee599ca9b194942e5ae
outcastgeek <outcastgeek+git@gmail.com>
2013-01-02 22:25:40 -0600

    Updated spec.

--- spec/jrzmq_spec.rb
+++ spec/jrzmq_spec.rb
@@ -1,44 +1,48 @@
 require 'spec_helper'
 
 describe ZMQ do
-  context "#UPSTREAM_DOWNSTREAM" do
+  context "UPSTREAM_DOWNSTREAM" do
+
+    before(:each) do
+      @context = ZMQ::Context.new(1)
+
+      puts "Opening connection for READ DOWNSTREAM"
+      @inbound = @context.socket(ZMQ::UPSTREAM)
+      @inbound.bind("ipc://localconnection")
+      @inbound.set_receive_time_out 10
+
+      puts "Opening connection for WRITE UPSTREAM"
+      @outbound = @context.socket(ZMQ::DOWNSTREAM)
+      @outbound.connect("ipc://localconnection")
+      @outbound.set_send_time_out 10
+    end
+
+    after(:each) do
+      @inbound.close
+      @outbound.close
+      @context.term
+    end
+    
     it "sends two messages UPSTREAM and receives two messages DOWNSTREAM" do
-      
-      updown = Thread.new do
-        context = ZMQ::Context.new(1)
-
-        puts "Opening connection for READ DOWNSTREAM"
-        inbound = context.socket(ZMQ::UPSTREAM)
-        inbound.bind("ipc://localconnection")
-        inbound.set_receive_time_out 10
-
-        puts "Opening connection for WRITE UPSTREAM"
-        outbound = context.socket(ZMQ::DOWNSTREAM)
-        outbound.connect("ipc://localconnection")
-        outbound.set_send_time_out 10
-
-        messages = %w{Hello  World! QUIT}
-        received_msgs = []
+      messages = %w{Hello  World! QUIT}
+      received_msgs = []
         
-        messages.each do |msg|
-          outbound.send(msg)
-        end
-
-        loop do
-          received_msg = inbound.recv_str
-          puts "Received #{received_msg}"
-          received_msgs << received_msg if received_msg != nil
-          break if received_msg == "QUIT"
-        end
-
-        received_msgs.length.should == messages.length
-        
-        for i in 0..messages.length
-          received_msgs[i].should == messages[i]
-        end
+      messages.each do |msg|
+        @outbound.send(msg)
       end
 
-      updown.join
+      loop do
+        received_msg = @inbound.recv_str
+        puts "Received #{received_msg}"
+        received_msgs << received_msg if received_msg != nil
+        break if received_msg == "QUIT"
+      end
+
+      received_msgs.length.should == messages.length
+        
+      for i in 0..messages.length
+        received_msgs[i].should == messages[i]
+      end
     end
   end
 end
